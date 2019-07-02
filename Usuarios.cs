e2ec05f338b16af20b9b0196bdc79241d69199df
Odrazul <51931372+Odrazul@users.noreply.github.com>
2019-07-02 13:31:30 -0400

    Add files via upload

---
+++ Usuarios.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConexionesDirectorios.Models
+{
+    public class Usuarios
+    {
+        [JsonProperty(PropertyName = "class")]
+        public string className { get; set; }
+        public List<NodeDataArray> nodeDataArray { get; set; }
+    }
+
+
+   public class NodeDataArray
+    {
+        public int key { get; set; }
+        public string name { get; set; }
+        public string title { get; set; }
+        public int? parent { get; set; }
+    }
+}
