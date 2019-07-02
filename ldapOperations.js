7eaf405e76864fa45cca1b019cac9bb792fb01e4
luisllerena <llerenal@gmail.com>
2019-07-02 16:05:24 -0500

    Create ldapOperations.js

---
+++ ldapOperations.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $("#btnValidate").click(function () {
+        var userJson = $("#txtUser").val();
+        $.ajax({
+            type: "POST",
+            url: "/Home/ValidateLdapUser",
+            data: { "user": userJson },
+            success: function (result) {
+                if (result) {
+                    alert("Usuario existe");
+                } else {
+                    alert("Usuario No existe");
+                }
+            }
+        });
+
+    });
+});
